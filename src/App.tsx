df5f29b21b6474235e1ee13cf35f1ff194383e23
AhmedElachry <ahmedelashry.yt@gmail.com>
2025-10-05 07:27:56 +0300

    feat: add routing and pages for Home and Favorites

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,26 @@
+import { Routes, Route, Link } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import FavouritesPage from "./pages/FavouritesPage";
+
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline ">Github users app</h1>
-    </>
+    <div className="min-h-dvh">
+      <nav className="p-4 border-b border-gray-300">
+        <Link to="/" className="p-4">
+          Home
+        </Link>
+        <Link className="p-4" to="/favorites">
+          Favorites
+        </Link>
+      </nav>
+
+      <main className="p-4">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/favorites" element={<FavouritesPage />} />
+        </Routes>
+      </main>
+    </div>
   );
 }
 
